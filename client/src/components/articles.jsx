ebefa1e76fc47c9cb2a0667205a4ae5abc16a87e
supersowbow <kybow@protonmail.com>
2018-09-30 22:32:46 -0500

    created a new app in the client

---
+++ client/src/components/articles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Articles = () => {
+  return (
+    <div className="main">
+      <h1>Articles</h1>
+    </div>
+  );
+};
+
+export default Articles;
---
+++ client/src/components/mainContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+
+import BurgerButton from './burgerbutton.jsx'
+import Home from './home.jsx';
+import About from './about.jsx';
+import Testimonial from './testimonial.jsx';
+import Signup from './signup.jsx';
+import Articles from './/articles.jsx';
+
+import './css/mainContainer.css';
+
+const MainContainer = () => {
+  return (
+    <div className="main-container">
+      <BurgerButton showSideBar={this.handleShowSideBar}/>
+      {/* Wrap the CSS Transition Group in React Transition Group*/}
+      {/* Wrap the Switch in CSS Transition Group */}
+      <Switch>
+          {/* React Routes */}
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/testimonial' component={Testimonial} />
+          <Route path='/signup' component={Signup} />
+          <Route path='/articles' component={Articles} />
+          {/* End of React Routes */}
+      </Switch>
+    </div>
+  );
+};
+
+export default MainContainer;

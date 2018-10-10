1cb2ecec75579ad6bb31b8c6d23b867c1547d82a
supersowbow <kybow@protonmail.com>
2018-10-10 17:52:24 -0500

    added form

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 
 import './home.css';
 
-/*
-<img src={require('../../img/homeLogo_800w.png')}
-     sizes="100vw"
-     srcset={require('../../img/homeLogo_480w.png 480w, ../../img/homeLogo_800w.png 800w')}/>
-
-     <img alt="Pat Gavin's Basketball Fundamentals Camp Logo"
-          src={require('../../img/homeLogo_280w.png')}/>
-*/
-
 const Home = () => {
   return (
     <div className="home-content-container">
--- client/src/components/Signup/signup.jsx
+++ client/src/components/Signup/signup.jsx
@@ -4,8 +4,54 @@ import './signup.css';
 
 const Signup = () => {
   return (
-    <div className="main">
-      <h1>Sign Up</h1>
+    <div className="signup-container">
+        <form action=""
+              method="post"
+              className="signup-form"
+              onSubmit="">
+
+            <div>
+                <label htmlFor="guardian_name">Guardian's Full Name</label>
+                <input required
+                       type="text"
+                       id="guardian_name"
+                       name="guardian_name"
+                       placeholder=""/>
+            </div>
+
+            <div>
+                <label htmlFor="guardian_email">Guardian's Email</label>
+                <input required
+                       type="email"
+                       id="guardian_email"
+                       name="guardian_email"
+                       placeholder="Email"/>
+            </div>
+
+            <div>
+                <label htmlFor="phone-number">Guardian's Phone Number</label>
+                <input required
+                       type="text"
+                       id="phone-number"
+                       name="phone-number"
+                       placeholder="555-555-5555"/>
+            </div>
+
+            <div>
+                <label htmlFor="child_name">Child's Full Name</label>
+                <input required
+                       type="text"
+                       id="child_name"
+                       name="child_name"
+                       placeholder="Child's Full Name"/>
+            </div>
+
+            <input required
+                   type="submit"
+                   value="Submit"
+                   id="submit-button"
+                   name="submit_button"/>
+        </form>
     </div>
   );
 };

1f2e6177cfaa46c4e95721775189442d78b663d9
supersowbow <kybow@protonmail.com>
2019-01-08 10:14:49 -0600

    updated mobile design for home page

--- src/client/src/components/Home/home.jsx
+++ src/client/src/components/Home/home.jsx
@@ -7,18 +7,24 @@ const Home = () => {
   return (
     <div className="home-content-container">
       <div className="header-container">
-        <h2>Pat Gavin's</h2>
-        <img src="https://via.placeholder.com/150" alt="placeholder"/>
-        <h2>Basketball Fundamentals</h2>
-        <h2>Camp</h2>
-      </div>
-      <div className="learn-more">
-          <Link to="/about">
-            <button>
-              Learn More
-            </button>
-          </Link>
+        <div className="header-title-1">
+          <h2 className="ht1-item">Pat Gavin's</h2>
         </div>
+        <div className="header-img">
+          <img src="https://via.placeholder.com/150" 
+               alt="placeholder"
+               className="hi-item"
+          />
+        </div>
+        <div className="header-title-2">
+          <h2 className="ht2-item-1">Basketball</h2>
+          <h2 className="ht2-item-2">Fundamentals</h2>
+          <h2 className="ht2-item-3">Camp</h2>
+        </div>
+      </div>
+      <div className="learn-more-container">
+        <Link to="/about" className="button-link">Learn More</Link>
+      </div>
     </div>
   );
 };
@@ -29,6 +35,17 @@ const Home = () => {
         className="home-logo"
         alt="Pat Gavin's Basketball Fundamentals Camp Logo"/>
   </div>
+
+  CODE FOR LEARN MORE BUTTON
+  <Link to="/about">
+    <button>
+      Learn More
+    </button>
+  </Link>
+
+          <button className="button">
+          <Link to="/about" className="button-link">Learn More</Link>
+        </button>
 */
 
 export default Home;
